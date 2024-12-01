46101cdb3c2a37c854c9b4c7c73d6c19c98275c4
ryanw-clark <185879579+ryanw-clark@users.noreply.github.com>
2024-11-30 20:22:18 -0800

    Fixed SNMP

--- Linux/Ubuntu/zabbix.sh
+++ Linux/Ubuntu/zabbix.sh
@@ -78,3 +78,18 @@ sudo apt install snmp
 sudo apt install snmp-mibs-downloader
 snmpwalk -v2c -c public 10.2.1.1
 sudo nmap -sU -p 161 10.2.1.1
+
+# Issue: No templates in Zabbix on Admin superuser account
+# Remediation: verified configuration, dropped zabbix table, recreated, resolved
+sudo mysql -u root -p
+create database zabbix character set utf8mb4 collate utf8mb4_bin;
+create user zabbix@localhost identified by 'capstone'; # user already exists unnecessary
+grant all privileges on zabbix.* to zabbix@localhost;
+set global log_bin_trust_function_creators = 1;
+quit;
+
+sudo zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix
+
+sudo mysql -u root -p
+set global log_bin_trust_function_creators = 0;
+quit;
